7cc91d80071d2a143c02ec39a871311a75d6237f
JennyMcFlyy <jennyrambla@gmail.com>
2021-08-15 18:37:16 -0300

    agregando codigo a probando.js en la nueva rama cre

--- trabajogrupal01/probando.js
+++ trabajogrupal01/probando.js
@@ -1,2 +1,14 @@
 console.log ("Hola este es el primer achivo");
 console.log("estoy modificando este archivo de nuevo! Podes agregarle cambios tambien vos");
+
+
+//hola esto esta en la nueva branch y no en el main
+
+let horariosPartida = [12, 14, 18, 21];
+
+let horariosAtrasados = horariosPartida.map (function(horario){
+return horario-1;
+}
+)
+
+console.log(horariosAtrasados);
