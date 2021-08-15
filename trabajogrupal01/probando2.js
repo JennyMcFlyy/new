a22aa4604e7f9dcfaf9ca6de67c89cb5000a5609
JennyMcFlyy <jennyrambla@gmail.com>
2021-08-15 18:18:33 -0300

    agregue codigo a probando2 a ver que pasa si vos modificas el codigo o agregas algo de codigo en el medio

--- trabajogrupal01/probando2.js
+++ trabajogrupal01/probando2.js
@@ -1,1 +1,28 @@
 console.log("Hola este es el segundo");
+
+//agrego un pedazo de codigo aca a ver si vos modificas algo adentro del codigo que pasa
+
+
+let arrayDeWebs = [ "www.yahoo.com", "www.google.com", "www.facebook.com", "www.instagram.com"]
+
+function agregarHttp (url){
+  return "http://"+url;
+}
+
+//console.log(agregarHttp("www.playground.com"));
+let arrayDeWebHttp = [];
+
+function procesar (arrayDeWebs, agregarHttp){
+  for (i=0; i < arrayDeWebs.length; i++){
+   let webHttp = agregarHttp (arrayDeWebs[i]);
+      arrayDeWebHttp.push (webHttp);
+  }
+  return arrayDeWebHttp;
+}
+
+console.log(arrayDeWebs.length);
+console.log(arrayDeWebs);
+
+console.log(procesar(arrayDeWebs, agregarHttp));
+console.log(arrayDeWebHttp.length);
+
